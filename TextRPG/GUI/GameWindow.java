ecfb69bde185457c0b54cc76e0fe75f6df1b60f5
mtoomeydev@gmail.com <mtoomeydev@gmail.com>
2021-05-02 23:42:52 -0400

    main menu layout additions, exit button functional, tweaked logic for switching menu layouts

--- TextRPG/GUI/GameWindow.java
+++ TextRPG/GUI/GameWindow.java
@@ -6,6 +6,9 @@ import java.awt.Frame;
 import java.awt.Toolkit;
 import java.awt.Window;
 import java.awt.event.*;
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Label;
 
 /*
 - Main window where game components will be arranged
@@ -32,7 +35,7 @@ public class GameWindow extends Frame{
                     System.exit(0);
                 }
             });
-
+            
             // center the window on the screen
             centerScreen();
 
@@ -48,12 +51,39 @@ public class GameWindow extends Frame{
             });
     }
     
+    /*
+    * handles selection of various menu states/layouts
+      Main: starting menu with inputs to start, load, or exit
+      Game: main gameplay view, has inputs to save, load, return to menu
+    */
     public void menuSequence(String menuSelection)
     {
-        menuSelection.toLowerCase();
+        menuSelection = menuSelection.toLowerCase();
 
         if(menuSelection.equals("main")){
             System.out.println("Main Menu");
+            setLayout("main");
+        }
+        else if(menuSelection.equals("game")){
+            System.out.println("Gameplay Screen");
+        }
+        else if(menuSelection.equals("exit")){
+            System.out.println("Closing..");
+            System.exit(0);
+        }
+        else System.out.println("Invalid selection.");
+    }
+
+    /*
+    * handles setting up different layouts for the window
+    */
+    public void setLayout(String layout){
+        layout.toLowerCase();
+        
+        switch(layout) {
+            case "main":
+                setupMainMenuLayout(gameArea);
+                break;
         }
     }
 
@@ -66,15 +96,44 @@ public class GameWindow extends Frame{
         this.setLocation(xLocation, yLocation);
     }
 
+    public void setupMainMenuLayout(Window w){
+        
+        Label header = new Label("Main Menu");
+        header.setBounds(345, 100, 100, 50);
+
+        Button start = new Button("Start");
+        start.setBounds(325, 200, 100, 50);
+
+        Button load = new Button("Load");
+        load.setBounds(325, 300, 100, 50);
+
+        Button exit = new Button("Exit");
+        // exit button functionality
+        exit.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.exit(0);
+            }
+        });
+
+        exit.setBounds(325, 400, 100, 50);
+
+        w.add(header);
+        w.add(start);
+        w.add(load);
+        w.add(exit);
+    }
+
     /* window within the main frame to display game contents
     */
     class DisplayWindow extends Window{
         public DisplayWindow(Frame container, int status){
             super(container);
+            
+            // setup with default values
             setSize(750, 900);
-            setLocationRelativeTo(container);
+            setLocationRelativeTo(container); // location should always be relative to parent container
             setBackground(Color.WHITE);
-
+            setLayout(null);
         }
 
             /* updates location relative to the GameWindow
